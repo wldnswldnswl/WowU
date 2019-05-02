b41575cc3af4ae5063eb8535b602438169081de6
eom1224 <eomjisoo@gmail.com>
2019-05-02 22:14:44 +0900

    test

--- app/src/main/java/com/capstone/wowu/MainActivity.java
+++ app/src/main/java/com/capstone/wowu/MainActivity.java
@@ -17,7 +17,6 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
