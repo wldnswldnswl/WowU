adef0b8f257de347cce8d74323f2839035d1e3a4
eom1224 <eomjisoo@gmail.com>
2019-05-08 19:05:37 +0900

    js

--- app/src/main/java/com/capstone/wowu/LoginActivity.java
+++ app/src/main/java/com/capstone/wowu/LoginActivity.java
@@ -21,7 +21,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, com.capstone.wowu.MainActivity.class);
-                startActivity(intenteeeeee);
+                startActivity(intent);
             }
         });
 
